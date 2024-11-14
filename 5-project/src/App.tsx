f03be08fd20f7c7c2d15a574a6f0665e27a1dd08
Dawid <dawid1234x@wp.pl>
2024-11-14 13:50:30 +0100

    feat(5): init

---
+++ 5-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material"
+import { ImageEditor } from "./component/ImageEditor/ImageEditor"
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <ImageEditor />
+    </Container>
+  )
+}
+
+export default App
